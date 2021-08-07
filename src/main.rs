39e3d31f6001345bf6ca993014acddc218ab1551
BruceTian <brucetianx@gmail.com>
2021-08-07 23:47:33 +0800

    feat: add sbi

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
 #![no_std]
 #![no_main]
+#![feature(global_asm)]
 #![feature(llvm_asm)]
+#![feature(panic_info_message)]
+use core::fmt::{self, Write};
 
 mod lang_items;
 
+mod sbi;
 
 const SYSCALL_EXIT: usize = 93;
+
+
+
+global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {    
+    loop {}
+}
+
 
 fn syscall(id: usize, args: [usize; 3]) -> isize {
     let mut ret: isize;
@@ -24,7 +38,39 @@ pub fn sys_exit(xstate: i32) -> isize {
     syscall(SYSCALL_EXIT, [xstate as usize, 0, 0])
 }
 
-#[no_mangle]
-extern "C" fn _start() {
-    sys_exit(9);
+
+
+const SYSCALL_WRITE: usize = 64;
+
+pub fn sys_write(fd: usize, buffer: &[u8]) -> isize {
+  syscall(SYSCALL_WRITE, [fd, buffer.as_ptr() as usize, buffer.len()])
+}
+
+
+
+struct Stdout;
+
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        sys_write(1, s.as_bytes());
+        Ok(())
+    }
+}
+
+pub fn print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
+}
+
+#[macro_export]
+macro_rules! print {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        $crate::console::print(format_args!($fmt $(, $($arg)+)?));
+    }
+}
+
+#[macro_export]
+macro_rules! println {
+    ($fmt: literal $(, $($arg: tt)+)?) => {
+        print(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+    }
 }
