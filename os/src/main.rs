cdd9c6253ac15b384816019fdd60bda4ce342046
brucetian <brucetianx@gmail.com>
2022-05-10 00:09:58 +0800

    impl console

--- os/src/main.rs
+++ os/src/main.rs
@@ -2,9 +2,22 @@
 #![no_main]
 
 mod lang_items;
+mod sbi;
+
 use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
 
-fn main() {
-    
+#[no_mangle]
+fn rust_main() {
+    clear_bss();
+    loop {}
+}
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| 
+        unsafe { (a as *mut u8).write_volatile(0) }
+    );
 }
