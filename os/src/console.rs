4bc367601062025d5dd30e78f72f36185e597e29
brucetian <brucetianx@gmail.com>
2022-05-10 00:43:07 +0800

    console

--- os/src/console.rs
+++ os/src/console.rs
@@ -1,10 +1,12 @@
+//! SBI console driver, for text output
+
 use crate::sbi::console_putchar;
-use core::fmt::{self,Write};
+use core::fmt::{self, Write};
 
 struct Stdout;
 
-impl Write for Stdout{
-    fn write_str(&mut self,s: &str)-> fmt::Result{
+impl Write for Stdout {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
         for c in s.chars() {
             console_putchar(c as usize);
         }
@@ -12,8 +14,8 @@ impl Write for Stdout{
     }
 }
 
-pub fn print(args : fmt::Arguments){
-    Stdout.write_str(args).unwrap();
+pub fn print(args: fmt::Arguments) {
+    Stdout.write_fmt(args).unwrap();
 }
 
 /// print string macro
--- os/src/main.rs
+++ os/src/main.rs
@@ -1,6 +1,20 @@
+//! The main module and entrypoint
+//!
+//! The operating system and app also starts in this module. Kernel code starts
+//! executing from `entry.asm`, after which [`rust_main()`] is called to
+//! initialize various pieces of functionality [`clear_bss()`]. (See its source code for
+//! details.)
+//!
+//! We then call [`println!`] to display `Hello, world!`.
+
+#![deny(missing_docs)]
+#![deny(warnings)]
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
 
+#[macro_use]
+mod console;
 mod lang_items;
 mod sbi;
 
@@ -10,7 +24,8 @@ global_asm!(include_str!("entry.asm"));
 #[no_mangle]
 fn rust_main() {
     clear_bss();
-    loop {}
+    println!("Hello, world!");
+    panic!("Shutdown machine!");
 }
 fn clear_bss() {
     extern "C" {
