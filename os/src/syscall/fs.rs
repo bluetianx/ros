0d86bc9692767ccd82bf535c69a0965f5d0e76ff
brucetian <brucetianx@gmail.com>
2022-06-27 22:04:02 +0800

    feat: update syscall

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -1,7 +1,5 @@
 //! File and filesystem-related syscalls
 
-use log::{error, info};
-
 const FD_STDOUT: usize = 1;
 
 /// write buf of length `len`  to a file with `fd`
@@ -10,12 +8,11 @@ pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
         FD_STDOUT => {
             let slice = unsafe { core::slice::from_raw_parts(buf, len) };
             let str = core::str::from_utf8(slice).unwrap();
-            info!("{}", str);
+            print!("{}", str);
             len as isize
         }
         _ => {
-            error!("Unsupported fd in sys_write!");
-            len as isize
+            panic!("Unsupported fd in sys_write!");
         }
     }
 }
