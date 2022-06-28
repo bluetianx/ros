eee9b976a4bea234ccac851ba5cb9900d4fd193e
brucetian <brucetianx@gmail.com>
2022-06-29 00:37:13 +0800

    update

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,20 +1,37 @@
 //! The main module and entrypoint
 //!
-//! The operating system and app also starts in this module. Kernel code starts
+//! Various facilities of the kernels are implemented as submodules. The most
+//! important ones are:
+//!
+//! - [`trap`]: Handles all cases of switching from userspace to the kernel
+//! - [`task`]: Task management
+//! - [`syscall`]: System call handling and implementation
+//!
+//! The operating system also starts in this module. Kernel code starts
 //! executing from `entry.asm`, after which [`rust_main()`] is called to
-//! initialize various pieces of functionality [`clear_bss()`]. (See its source code for
+//! initialize various pieces of functionality. (See its source code for
 //! details.)
 //!
-//! We then call [`println!`] to display `Hello, world!`.
+//! We then call [`task::run_first_task()`] and for the first time go to
+//! userspace.
 
 #![deny(missing_docs)]
-//#![deny(warnings)]
+#![deny(warnings)]
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
+
+use core::arch::global_asm;
+
+#[cfg(feature = "board_k210")]
+#[path = "boards/k210.rs"]
+mod board;
+#[cfg(not(any(feature = "board_k210")))]
+#[path = "boards/qemu.rs"]
+mod board;
 #[macro_use]
 mod console;
-pub mod batch;
+mod config;
 mod lang_items;
 mod loader;
 mod sbi;
@@ -24,30 +41,10 @@ pub mod task;
 mod timer;
 pub mod trap;
 
-mod logging;
-
-use log::{info, trace, debug, warn,error};
-use logging::init;
-use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
 
-#[no_mangle]
-fn rust_main() {
-    clear_bss();
-    init();
-
-    info!("kernel Hello, world!");
-
-    trap::init();
-    loader::load_apps();
-    trap::enable_timer_interrupt();
-    timer::set_next_trigger();
-    task::run_first_task();
-    panic!("Unreachable in rust_main!");
-
-}
-
+/// clear BSS segment
 fn clear_bss() {
     extern "C" {
         fn sbss();
@@ -57,4 +54,17 @@ fn clear_bss() {
         core::slice::from_raw_parts_mut(sbss as usize as *mut u8, ebss as usize - sbss as usize)
             .fill(0);
     }
+}
+
+/// the rust entry-point of os
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("[kernel] Hello, world!");
+    trap::init();
+    loader::load_apps();
+    trap::enable_timer_interrupt();
+    timer::set_next_trigger();
+    task::run_first_task();
+    panic!("Unreachable in rust_main!");
 }
